36d9e01a945c474e80f3bcb46075b0a1b902f440
leizhonghua <leizh199412@163.com>
2019-07-09 16:22:02 +0800

    分支1进行了修改

--- src/main/java/com/lei/git/MyClass.java
+++ src/main/java/com/lei/git/MyClass.java
@@ -2,6 +2,6 @@ package com.lei.git;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Git推送测试,分支1进行了操作");
+        System.out.println("分支1进行了修改。。。");
     }
 }
