d7874f7626046faf62a42f2865e1a18e09809a6d
leizhonghua <leizh199412@163.com>
2019-07-09 16:20:23 +0800

    分支1进行了操作

--- src/main/java/com/lei/git/MyClass.java
+++ src/main/java/com/lei/git/MyClass.java
@@ -2,6 +2,6 @@ package com.lei.git;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Git推送测试");
+        System.out.println("Git推送测试,分支1进行了操作");
     }
 }
